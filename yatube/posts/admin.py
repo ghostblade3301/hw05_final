9be39e5576cfa5fc5ba573f8d4397ada1a312cd9
Pavel Naumenko <pavelnaumenkowork@gmail.com>
2023-02-17 08:08:30 +0300

    review v3

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Group, Post
+from .models import Group, Post, Comment, Follow
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -28,3 +28,5 @@ class GroupAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment)
+admin.site.register(Follow)
